7eafe2cece0f474dd68d87a937a9ea9f52393c4b
Imanina Majeed <imanina@recovision.net>
2022-12-30 14:59:38 +0800

    Add historical data page component

--- client/src/Pages/HistoricalData.js
+++ client/src/Pages/HistoricalData.js
@@ -1,28 +1,49 @@
-//CSS libraries
-// import "../Assets/CSS/LiveResult.css";
-
 //Components
 import ImageLP from "../Components/HistoricalData/ImageLP";
 import ImageRTSP from "../Components/HistoricalData/ImageRTSP";
 
+let mqttGlobalClientData = [];
 export default function LiveResult(props) {
   let data = props.data;
-  let vehicleImagePath = data.vehicleImagePath;
-  let plateImagePath = data.plateImagePath;
-  let plateNum = data.plateNum;
-  let confidenceLevel = data.confidenceLevel;
-  let createdAt = data.createdAt;
+
+  if (mqttGlobalClientData[0] === "null") {
+    mqttGlobalClientData[0] = data;
+  } else {
+    mqttGlobalClientData.push(data);
+  }
 
   return (
     <div className="App">
       <h1>Historical Data</h1>
-      <ImageRTSP path={vehicleImagePath}></ImageRTSP>
-      <ImageLP
-        path={plateImagePath}
-        plate={plateNum}
-        confidenceLevel={confidenceLevel}
-        timeStamp={createdAt}
-      ></ImageLP>
+      <table className="table table-hover table-secondary">
+        <thead>
+          <tr>
+            {/* <th scope="col">#</th> */}
+            <th scope="col">Timestamp</th>
+            <th scope="col">Captured Vehicle</th>
+            <th scope="col">Captured Number Plate</th>
+            <th scope="col">Detected Plate Number</th>
+            <th scope="col">Confidence Level (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mqttGlobalClientData.map((mqttData) => (
+            <tr>
+              <th scope="row" key={mqttData}>
+                {mqttData.createdAt}
+              </th>
+              <td>
+                <ImageRTSP path={mqttData.vehicleImagePath}></ImageRTSP>
+              </td>
+              <td>
+                <ImageLP path={mqttData.plateImagePath}></ImageLP>
+              </td>
+              <td>{mqttData.plateNum}</td>
+              <td>{mqttData.confidenceLevel}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
