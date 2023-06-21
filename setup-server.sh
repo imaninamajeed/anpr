bee6602a5b26b189cd70648492c6bad15713a5c7
Ho Zi Xuan <zixuanho@recogine.com>
2023-06-21 11:34:00 +0800

    build: update server setup script

--- setup-server.sh
+++ setup-server.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 set -e
+# shellcheck source=/home/rnd/.bashrc
+source ~/.bashrc
 
+echo "Setting up server..."
 # >> ~/.bashrc
-echo "" >>~/.bashrc
-echo "# export IPv4 to env" >>~/.bashrc
-echo "export DOCKER_GATEWAY_HOST=\$(hostname -I | awk '{ print \$1 }')" >>~/.bashrc
+if [[ -z "${DOCKER_GATEWAY_HOST}" ]]; then
+    echo "DOCKER_GATEWAY_HOST is not set!"
+    echo "Proceed to setting up the variable..."
+
+    echo "" >>~/.bashrc
+    echo "# export IPv4 to env" >>~/.bashrc
+    echo "export DOCKER_GATEWAY_HOST=\$(hostname -I | awk '{ print \$1 }')" >>~/.bashrc
+else
+    echo "DOCKER_GATEWAY_HOST had been set!"
+fi
+echo "DOCKER_GATEWAY_HOST is $DOCKER_GATEWAY_HOST"
+echo "Done!"
